a5a888afa0f90a2625c60caca840e91023ed18af
Altronis <jihu.mun@stonybrook.edu>
2019-02-15 21:36:57 -0500

    changes to osuBoost

--- src/OsuBoost.java
+++ src/OsuBoost.java
@@ -1,11 +1,17 @@
 import java.awt.*;
-import java.awt.event.KeyEvent;
 
 public class OsuBoost {
-
-    public static Robot robot;
-
     public static void main(String[] args) throws AWTException {
-        robot = new Robot();
+        int keys = 4; // Amount of keys
+        int x_start = 500; // starting x position
+        int x_gap = 120; // gap between x positions
+        int y_pos = 960; // y positions across all pixels
+
+        Point[] pixels = new Point[keys];
+
+        for (int i = 0; i < 3; i++) {
+            pixels[i] = new Point(x_start + x_gap * i, y_pos);
+            pixels[i].track();
+        }
     }
 }
