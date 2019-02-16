4791379636ff59e2b079f3099e8c6919226f647c
lvkv <lukas@rytas.io>
2019-02-15 21:23:40 -0500

    I forgot to finish this file lmao

--- src/Constants.java
+++ src/Constants.java
@@ -2,9 +2,7 @@ import java.awt.Color;
 public final class Constants {
 
     private Constants() {}
-//    regular notes: R 53 G 200 B 255
-//    holds: R 0 G 255 B 0
+    
     public static final Color NOTE = new Color(53,200, 255); // Light blue
-    public static final Color NOTE_HOLD = new Color(0,255, 0); //
-
+    public static final Color NOTE_HOLD = new Color(0,255, 0); // Green
 }
