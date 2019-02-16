f03034475d94c0bc8bb5ed54eadba0e0b8dffe83
lvkv <lukas@rytas.io>
2019-02-15 21:56:40 -0500

    screen reading and key pressing

--- src/Constants.java
+++ src/Constants.java
@@ -1,8 +1,11 @@
 import java.awt.Color;
+import java.awt.event.KeyEvent;
+
 public final class Constants {
 
     private Constants() {}
 
     public static final Color NOTE = new Color(53,200, 255); // Light blue
     public static final Color NOTE_HOLD = new Color(0,255, 0); // Green
+
 }
--- src/Point.java
+++ src/Point.java
@@ -1,29 +1,50 @@
 import java.awt.*;
 
-public class Point {
-    private Robot robot;
+public class Point implements Runnable{
+    private Robot robot; // Handles reading screen and user input
     private int x; // x position of pixel
     private int y; // y position of pixel
+    private int keyCode;
+    private boolean isHeld;
 
-    public Point(int x, int y) throws AWTException {
+
+    public Point(int x, int y, int keyCode) throws AWTException {
         this.x = x;
         this.y = y;
+        this.keyCode = keyCode;
         robot = new Robot();
     }
 
-    public int getX() {
-        return x;
+    @Override
+    public void run() {
+        Color current_color = getColor();
+        if (isHeld) { // If we're sustained
+            if (current_color.equals(Color.BLACK)) { // Black background means stop sustaining
+                release();
+                isHeld = false;
+            }
+            else
+                return; // Else, continue sustaining
+        }
+        else if (current_color.equals(Constants.NOTE)){  // If we see a single note, hit the key
+            press();
+            release();
+        }
+        else if (current_color.equals(Constants.NOTE_HOLD)){ // If we see a sustained note, press the key without releasing
+            press();
+            isHeld = true;
+        }
     }
 
-    public int getY() {
-        return y;
+    public void press() {
+        robot.keyPress(keyCode);
     }
 
-    public void track() {
-
+    public void release() {
+        robot.keyRelease(keyCode);
     }
 
-    public Color getColor() {
+    private Color getColor() {
         return robot.getPixelColor(this.x, this.y);
     }
 }
