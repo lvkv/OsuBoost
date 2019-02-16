c94defb9cda7a1b509bfa2e76aec1f4de198f495
Altronis <jihu.mun@stonybrook.edu>
2019-02-16 00:57:41 -0500

    start and stop methods

--- src/OsuBoost.java
+++ src/OsuBoost.java
@@ -5,18 +5,28 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class OsuBoost {
-    public static void main(String[] args) throws AWTException {
-        int keys = 4; // Amount of keys
-        int x_start = 500; // starting x position
-        int x_gap = 120; // gap between x positions
-        int y_pos = 960; // y positions across all pixels
+    int keys = 4; // Amount of keys
+    int x_start = 500; // starting x position
+    int x_gap = 120; // gap between x positions
+    int y_pos = 960; // y positions across all pixels
+    int[] keyCodes = {KeyEvent.VK_D, KeyEvent.VK_F, KeyEvent.VK_J, KeyEvent.VK_K};
+    boolean running = false; // osu is running
+    ScheduledExecutorService executor;
 
-        int[] keyCodes = {KeyEvent.VK_D, KeyEvent.VK_F, KeyEvent.VK_J, KeyEvent.VK_K};
+    public void start() throws AWTException {
+        if (!running) {
+            executor = Executors.newScheduledThreadPool(keys);
 
-        ScheduledExecutorService executor = Executors.newScheduledThreadPool(keys);
+            for (int i = 0; i <= 3; i++)
+                executor.scheduleAtFixedRate(new Point(x_start + x_gap * i, y_pos, keyCodes[i]), 0, 1, TimeUnit.MILLISECONDS);
+            running = true;
+        }
+    }
 
-        for (int i = 0; i <= 3; i++) {
-            executor.scheduleAtFixedRate(new Point(x_start + x_gap * i, y_pos, keyCodes[i]), 0, 1, TimeUnit.MILLISECONDS);
+    public void stop() {
+        if (running) {
+            executor.shutdownNow();
+            running = false;
         }
     }
 }
