72320f9423f992858794cad90142ab0c0df151c9
Altronis <jihu.mun@stonybrook.edu>
2019-02-18 19:57:53 -0500

    Fixed resolution bug

--- src/Constants.java
+++ src/Constants.java
@@ -9,7 +9,7 @@ public final class Constants {
     public static final String APP_NAME = "osu!Boost";
     public static final Color NOTE = new Color(53,200, 255); // Light blue
     public static final Color NOTE_HOLD = new Color(0,255, 0); // Green
-    public static final int FULL_HD = 1920;
+    public static final double FULL_HD = 1920;
 
     // GUI
     public static final String TITLE_FONT = "Tahoma";
